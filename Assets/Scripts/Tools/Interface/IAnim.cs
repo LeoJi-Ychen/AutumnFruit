214a7a4b38f3973e86df0f271b5d8cd699a964e1
LeoJi <492898584@qq.com>
2026-05-27 16:01:14 +0100

    delete

--- Assets/Scripts/Tools/Interface/IAnim.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-public interface IAnim
-{
-    void AnimPlay(GameObject obj,int id, float dt);
-    void SetSprites(string route);
-    void SetFrameTime(float t);
-}
