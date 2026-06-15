e19e92d4a87d2753f8f7e7259a55d37f3d336faf
kaiLin1 <120540004+kaiLin1@users.noreply.github.com>
2026-06-15 09:51:10 +0800

    615

--- Assets/Scripts/M/M_Count.cs
+++ Assets/Scripts/M/M_Count.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.Events;
+
 public class M_Count : MonoBehaviour
 {
     public List<GameObject> countlist;
     public int num;
     public TextMeshProUGUI display;
-    public List<GameObject> next;
+
+    [Header("完成后触发")]
+    public UnityEvent trigger;
+
     int state;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         if (display != null)
@@ -17,38 +22,34 @@ public class M_Count : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (state == 1)
         {
             return;
         }
+
         int n = 0;
-        foreach(GameObject obj in countlist)
+
+        foreach (GameObject obj in countlist)
         {
-            if (obj != null)
+            if (obj != null && obj.activeSelf)
             {
-                if (obj.activeSelf == true)
-                {
-                    n++;
-                }
+                n++;
             }
         }
+
         num = n;
+
         if (display != null)
         {
             display.text = "" + num;
         }
+
         if (n == countlist.Count)
         {
-            foreach(GameObject g in next)
-            {
-                if(g != null)
-                {
-                    g.SetActive(true);
-                }
-            }
+            trigger?.Invoke();
+
             state = 1;
         }
     }
