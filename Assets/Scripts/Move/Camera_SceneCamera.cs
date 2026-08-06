8f2f5a4d1d4861bf349442702f2df4132ea0ed0f
LeoJi <492898584@qq.com>
2026-08-06 17:37:45 +0100

    update

--- Assets/Scripts/Move/Camera_SceneCamera.cs
+++ Assets/Scripts/Move/Camera_SceneCamera.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Camera_SceneCamera : MonoBehaviour
 {
+    EventSystem es;
     public int state_move;
     public float speed;
     public GameObject endPoint;
     IUF uf = new UIFunctions();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Awake()
     {
-        
+        es = EventSystem.current;
     }
 
     // Update is called once per frame
@@ -21,12 +23,14 @@ public class Camera_SceneCamera : MonoBehaviour
             if (uf.Distance2(this.gameObject, endPoint) > 0.03 * speed)
             {
                 transform.Translate(uf.Direction2(this.gameObject, endPoint) * speed * Time.deltaTime);
+                es.enabled = false;
             }
             else
             {
                 state_move = 0;
                 transform.position
                 = new Vector3(endPoint.transform.position.x, endPoint.transform.position.y, transform.position.z);
+                es.enabled = true;
             }
         }
         else if(state_move == 2)
